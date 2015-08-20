ba91b175ee05b81c88937c39870d62412f6576b3
Abhilash <abhilash@abhilashs-iMac.local>
2015-08-20 12:50:03 +0530

    Initial Commit

---
+++ Protocols/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Protocols
+//
+//  Created by Abhilash on 20/08/15.
+//  Copyright (c) 2015 Abhilash Reddy Kallepu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
